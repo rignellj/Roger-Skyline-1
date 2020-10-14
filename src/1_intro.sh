8b0995eaaf85ac456f1ec893aa2c65e0d34b08b4
Jere Rignell <jrignell@student.hive.fi>
2020-10-14 14:07:51 +0300

    Add full path to intro file.sh

--- src/1_intro.sh
+++ src/1_intro.sh
@@ -3,17 +3,17 @@
 # This script updates & upgrades packages and also installs software
 # that is needed for this project
 
-apt install sudo && sudo apt update && sudo apt upgrade
+apt install sudo && /usr/bin/sudo apt update && /usr/bin/sudo apt upgrade
 
-printf "${WHITE}\nChecking partition${NC}"
+/usr/bin/printf "${WHITE}\nChecking partition${NC}"
 start_and_stop_animation dot 2
 
-sudo fdisk -l | grep Linux
+/usr/bin/sudo fdisk -l | /usr/bin/grep Linux
 
 read -p "Press enter to continue installing: " ENTER
 
-printf "${WHITE}\nInstalling ufw, portsentry, fail2ban, apache2, mailutils packages${NC}"
+/usr/bin/printf "${WHITE}\nInstalling ufw, portsentry, fail2ban, apache2, mailutils packages${NC}"
 start_and_stop_animation dot 3
-sudo apt-get install ufw portsentry fail2ban apache2 mailutils -y
+/usr/bin/sudo apt-get install ufw portsentry fail2ban apache2 mailutils -y
 
 STATUS_SYMBOLS[0]=${SUCCESS}
