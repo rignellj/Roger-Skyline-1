271ea8e68c1d1d69f42fa9b17a6d1ceeb579c8a0
Jere Rignell <jrignell@student.hive.fi>
2020-10-14 16:15:03 +0300

    crontab fix

--- src/10_cron_conf.sh
+++ src/10_cron_conf.sh
@@ -7,10 +7,11 @@ read -p "Type username to whom cron jobs to configure. User should have sudo pri
 /usr/bin/echo -ne "Add few tasks to crontab file"
 start_and_stop_animation dot 3
 
-UPDATE_4AM="0 4 * * 0 sudo /home/admin/scripts/update_packages.sh"
-UPDATE_AT_REBOOT="@reboot sudo /home/admin/scripts/update_packages.sh"
-MONITOR_CRON="0 0 * * * sudo /home/admin/scripts/cron_monitor.sh"
+UPDATE_4AM="0 4 * * 0 sudo /root/scripts/src/update_packages.sh"
+UPDATE_AT_REBOOT="@reboot sudo /root/scripts/src/update_packages.sh"
+MONITOR_CRON="0 0 * * * sudo /root/scripts/src/cron_monitor.sh"
 
+/usr/bin/crontab -u ${ANSWER} -e
 (/usr/bin/crontab -u ${ANSWER} -l; /usr/bin/echo "${UPDATE_4AM}" )| /usr/bin/crontab -u ${ANSWER} -
 (/usr/bin/crontab -u ${ANSWER} -l; /usr/bin/echo "${UPDATE_AT_REBOOT}") | /usr/bin/crontab -u ${ANSWER} -
 (/usr/bin/crontab -u ${ANSWER} -l; /usr/bin/echo "${MONITOR_CRON}") | /usr/bin/crontab -u ${ANSWER} -
